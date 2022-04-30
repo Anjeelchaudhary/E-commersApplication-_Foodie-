3aa3c431a9624d33dd1a5403544b745003075ae2
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-04-30 14:03:36 +0545

    :bug: fixed not showing image
    added suitable image

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommers_app/utils/colors.dart';
+import 'package:flutter_ecommers_app/widgets/big_text.dart';
+import 'package:flutter_ecommers_app/widgets/icon_and_text_widget.dart';
+import 'package:flutter_ecommers_app/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,11 +14,31 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageNodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      //color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -21,14 +47,88 @@ class _FoodPageNodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: const EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        image: const DecorationImage(image: AssetImage("assets/image/Sinki.png")),
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/image/chowmein.png")),
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven
+                ? const Color(0xFF69c5df)
+                : const Color(0xFF9294cc),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chowmein"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                              size: 15,
+                            );
+                          }),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "986"),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "Feedbacks"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: "1.2 Km",
+                            iconColor: AppColors.mainColor),
+                        IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: "20min",
+                            iconColor: AppColors.iconColor2)
+                      ],
+                    )
+                  ]),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
