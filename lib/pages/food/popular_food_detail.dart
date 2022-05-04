27d908174968a581c27c133bd29b80704adc6c84
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-04 16:10:26 +0545

    :memo: showing food in detail
    description of food is given

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommers_app/utils/dimensions.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(children: [
+        Positioned(
+          left: 0,
+          right: 0,
+          child: Container(
+            width: double.maxFinite,
+            height: Dimensions.popularFoodImgSize,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/image/momoo.png"))),
+          ),
+        ),
+        Positioned(
+          left: Dimensions.width20,
+          right: Dimensions.width20,
+          child: Row(
+            
+          ),
+        )
+      ]),
+    );
   }
 }
