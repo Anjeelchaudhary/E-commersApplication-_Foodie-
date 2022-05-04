0dddad7022c19224a13d2db4b2d8de24c325d2ea
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-04 18:52:16 +0545

    :art: added below description
    product detail

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommers_app/utils/colors.dart';
 import 'package:flutter_ecommers_app/utils/dimensions.dart';
+import 'package:flutter_ecommers_app/widgets/big_text.dart';
+import 'package:flutter_ecommers_app/widgets/icon_and_text_widget.dart';
+import 'package:flutter_ecommers_app/widgets/small_text.dart';
 
 import '../../widgets/app_icon.dart';
 
@@ -9,32 +13,107 @@ class PopularFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Positioned(
-          left: 0,
-          right: 0,
-          child: Container(
-            width: double.maxFinite,
-            height: Dimensions.popularFoodImgSize,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/image/momoo.png"))),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImgSize,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/image/momoo.png"))),
+            ),
           ),
-        ),
-        Positioned(
-          top: Dimensions.height45,
-          left: Dimensions.width20,
-          right: Dimensions.width20,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AppIcon(icon: Icons.arrow_back_ios),
-              AppIcon(icon: Icons.shopping_cart_outlined),
-            ],
+          Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
           ),
-        )
-      ]),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 20,
+            child: Container(
+              padding: EdgeInsets.only(
+                  left: Dimensions.width20,
+                  right: Dimensions.width20,
+                  top: Dimensions.height20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(Dimensions.radius20),
+                    topLeft: Radius.circular(Dimensions.radius20),
+                    
+                  ),
+                  color: Colors.white),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Noodle"),
+                    SizedBox(
+                      height: Dimensions.height10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                              size: 15,
+                            );
+                          }),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "986"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "Feedbacks"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: Dimensions.height20,
+                    ),
+                    //this is for the design ie normal,distance and for time
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: "1.2 Km",
+                            iconColor: AppColors.mainColor),
+                        IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: "20min",
+                            iconColor: AppColors.iconColor2)
+                      ],
+                    )
+                  ]),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
