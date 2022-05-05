774830b7e4a6f666b58986fd63c082eef6c5b0d1
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-05 12:14:05 +0545

    :pencil2: rounded corners is addedd
    rounded corner added

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommers_app/utils/colors.dart';
+import 'package:flutter_ecommers_app/utils/dimensions.dart';
+import 'package:flutter_ecommers_app/widgets/app_icon.dart';
+import 'package:flutter_ecommers_app/widgets/big_text.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
   const RecommendedFoodDetail({Key? key}) : super(key: key);
@@ -9,17 +13,55 @@ class RecommendedFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 70,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.clear),
+                AppIcon(icon: Icons.shopping_cart_outlined)
+              ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(20),
+              child: Container(
+                child: Center(
+                  child: BigText(
+                    size: Dimensions.font26,
+                    text: "Thakali Khana",
+                  ),
+                ),
+                width: double.maxFinite,
+                padding: EdgeInsets.only(top: 5, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(Dimensions.radius20),
+                    topRight: Radius.circular(Dimensions.radius20),
+                    
+                  ),
+                ),
+              ),
+            ),
+            pinned: true,
+            backgroundColor: AppColors.yelloColor,
+            expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
                 "assets/image/ThakaliKhana.png",
                 width: double.maxFinite,
-
-                
+                fit: BoxFit.cover,
               ),
             ),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+                "The Thakalis who are inhabitants of Thak Khola region of Mustang District is home to Thakali cuisine. The Thak-Khola Valley in ancient days used to be an easy trade route through the high Himalayas. And as trade was an important part of the Thakali people, they could import vegetables, and being in the higher Himalayan region most vegetables served were cooked in the dried version.Not to miss that this one cuisine happens to be less vegetarian in nature.A typical serving of Thakali plate would see Dal, Bhaat, Tarkari and Meat (lentil soup, rice, green vegetable) along with Kanchhemba (Buckwheat finger chips) the Mustang Alu topped with Jimbu (Himalayan leaf garlic) and a generous dollop of ghee (clarified butter). Though it’s low on desserts a sweet curd or Phopké (fermented rice) completes the meal. Vivek Sherchan, Owner, Jimbu Thakali by Capital Grill, who himself is a Thakali from Tukche village of Mustang Region says “the style of preparation of Thakali food is different due to the use of some ingredients that are characteristic of Thakali cuisine like Sukuti or dried goat/yak meat, Gundruk (fermented leafy green vegetables) with soybean along with green cabbage, buckwheat, barley flour and timbur, a spicy Himalayan spice. This gives the cuisine a whole new taste and flavour. The thali is not only appealing in taste but also visually with a variety of colours from the pale green dal, green leafy veggies, rich red meat, rice being white along with the chutneys like mula ko achaar (white radish pickle), chap/golbjeda ko chutney (fresh tomato pickle)”.The main meals are always served in a thali and variety is the trademark of thakali dishes. In Thakali homes cooking is done with mustard oil, or fresh ghee while in restaurants some dishes are cooked in Sunflower oil these days. Adding to the same Gauravman Sherchan, renowned photographer and CEO Jimbu Thakali who has many eyes for detail for this cuisine says “the best way to understand Nepal and its people is to take a dig into the Nepali cuisine. The authentic Nepali taste is found in Newari and Thakali cuisines out of which Thakali sees much richness and diversity. The cuisine is heavily influenced by the terrain where the Thakali lives eg- in between the higher Himalayan and low hilly region. Though the flavours are simple, the aroma of ghee in dal (lentil soup) and the distinct taste and smell of timur (zanthoxylum alayum) in curry makes this cuisine unforgettable."),
           )
         ],
       ),
     );
   }
 }
+
+ //margin: EdgeInsets.only(
+                   // left: Dimensions.width20, right: Dimensions.width20),
