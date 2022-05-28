1a5d129754ce7c3200e6fa206b062cf777fa94d2
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-28 14:50:23 +0545

    :bulb: Added commments
    comments is added in the code

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -3,9 +3,13 @@ import 'package:get/get.dart';
 import 'package:get/get.dart';
 
 class ApiClient extends GetConnect implements GetxService {
+  //here token is used because when you talk to the server
+  //we need to have the token and late key is for
+  //having late data transfer while communicating to server
   late String token;
-  final String appBaseUrl;
+  final String appBaseUrl; //this is server url
   late Map<String, String> _mainHeaders;
+  //this is the construtor
   ApiClient({required this.appBaseUrl}) {
     baseUrl = appBaseUrl;
     timeout = Duration(seconds: 30); // this is the timeout for the  request
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,1 +1,3 @@
+//here we are creating a method and is called a
+// init and aysnc is used because it has return type
 Future<void> init() async {}
